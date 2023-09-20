30d10109e040135775982c46c6294acd4defabb3
shobhit chaturvedi <113759547+chaturvedij@users.noreply.github.com>
2023-09-20 09:50:40 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int x,y,z;
+	    cin>>x>>y;
+	    
+	    if(x>=y)
+	    {
+	        cout<<x-y<<endl;
+	        
+	    }
+	    else
+	    {
+	        cout<<0<<endl;
+	    }
+	}
+	return 0;
+}
